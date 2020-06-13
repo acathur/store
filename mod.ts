e720a31ccf2beee725e4a508fb9a6c3f2f659e77
Acathur <chiahungzhang@gmail.com>
2020-06-13 13:40:41 +0800

    Lock depend `std:path` version @0.54.0

--- mod.ts
+++ mod.ts
@@ -1,4 +1,4 @@
-import { join } from 'https://deno.land/std/path/mod.ts'
+import { join } from 'https://deno.land/std@0.54.0/path/mod.ts'
 import { mkdir } from './utils.ts'
 
 export interface StoreOptions {
